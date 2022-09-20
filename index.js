9a5b4f56c2c09ae1a95e58726fb3367e67aaed5f
YERBE <yerbe.jordan@yahoo.fr>
2022-09-21 00:05:47 +0200

    deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 var port = process.env.PORT || 8000;
 
 
-server.listen(port, function() {
+app.listen(port, function() {
     console.log("App is running on port " + port);
 });
 
